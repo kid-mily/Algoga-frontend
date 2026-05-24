c272337b4cfc270b58f5897baa13ce4110e72d1f
jia40 <lsc7439@g.eulji.ac.kr>
2026-05-24 13:43:48 +0900

    [feature]공통모달구현#24

--- src/app/contentadmin/page.tsx
+++ src/app/contentadmin/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import Modal from "@/features/common/Modal";
+import CheckModal from "@/features/common/CheckModal";
+import CompleteModal from "@/features/common/CompleteModal";
+
 export default function ContentPage() {
+  const [open, setOpen] = useState(true);
+
   return (
     <div>
       콘텐츠 매니저 메인 페이지
+{/* 
+      <Modal
+        open={open}
+        onConfirm={() => console.log("확인")}
+        onCancel={() => setOpen(false)}
+      /> */}
+
+       {/* <CheckModal
+      open={open}
+      title="완료되었습니다"
+      description="등록이 완료되었습니다."
+      onConfirm={() => setOpen(false)}
+    /> */}
+
+
+    {/* <CompleteModal
+      open={open}
+      title="알림"
+      description="로그인에 성공하였습니다."
+      onConfirm={() => setOpen(false)}
+    /> */}
     </div>
   );
 }
