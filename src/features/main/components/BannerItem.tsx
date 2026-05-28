60e6b4d5e98382b3bf7d1bfd9e39c789bbec2404
als-wl <minnji852@naver.com>
2026-05-28 15:37:52 +0900

    [Feature] 메인 공지사항 백 연결#98

--- src/features/main/components/BannerItem.tsx
+++ src/features/main/components/BannerItem.tsx
@@ -1,18 +1,14 @@
-import Link from "next/link";
+  import Link from "next/link";
+import { Banner } from "./Types";
 
-interface BannerProps {
-  banner: {
-    id: number;
-    imageUrl: string;
-    linkUrl: string;
-  };
-}
-
-
-export default function BannerItem({ banner }: BannerProps) {
-  return (
-    <Link href={banner.linkUrl}>
-      <img src={banner.imageUrl} alt="배너" className="w-full h-full object-cover" />
-    </Link>
-  );
-}
+  interface BannerProps {
+    banner: Banner;
+    };
+  
+  export default function BannerItem({ banner }: BannerProps) {
+    return (
+      <Link href={banner.linkUrl}>
+        <img src={banner.imageUrl} alt="배너" className="w-full h-[200px] object-cover" />
+      </Link>
+    );
+  }
