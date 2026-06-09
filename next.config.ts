3eeae8d352af33cb63b425ec9c43b55cda60bbbc
als-wl <minnji852@naver.com>
2026-06-09 17:47:20 +0900

    도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "algoga-bucket.kro.kr",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
